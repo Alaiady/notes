fb42a79a705573ce51fb9da3bcaa69e561106552
Your Name <you@example.com>
2023-10-22 04:29:04 +0300

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,51 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:project_11/views/edit_notes_view.dart';
+import 'package:project_11/views/widgets/edit_view_body.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 18),
-      decoration: BoxDecoration(
-        color: const Color(0xFFFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(fontSize: 30, color: Colors.black),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Text(
-                'Flutter Developer Moon',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black.withOpacity(.5),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 18),
+        decoration: BoxDecoration(
+          color: const Color(0xFFFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(fontSize: 30, color: Colors.black),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'Flutter Developer Moon',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black.withOpacity(.5),
+                  ),
                 ),
               ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.black,
+                    size: 30,
+                  )),
             ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.delete,
-                  color: Colors.black,
-                  size: 30,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: Text(
-              'October 21,2023',
-              style:
-                  TextStyle(fontSize: 16, color: Colors.black.withOpacity(.5)),
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Text(
+                'October 21,2023',
+                style: TextStyle(
+                    fontSize: 16, color: Colors.black.withOpacity(.5)),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

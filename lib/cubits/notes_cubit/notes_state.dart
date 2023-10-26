17d267dd141d69ba60565747e3aba7d027dca8c9
Your Name <you@example.com>
2023-10-26 06:23:26 +0300

    display notes part one

--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -4,17 +4,3 @@ part of 'notes_cubit.dart';
 abstract class NotesState {}
 
 final class NotesInitial extends NotesState {}
-
-final class AddNoteLoding extends NotesState {}
-
-final class AddNoteSuccess extends NotesState {
-  final List<NoteModel> notes;
-
-  AddNoteSuccess(this.notes);
-}
-
-final class AddNoteFailure extends NotesState {
-  final String errMessage;
-
-  AddNoteFailure(this.errMessage);
-}
--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_11/cubits/notes_cubit/notes_cubit.dart';
+import 'package:project_11/models/note_model.dart';
 import 'package:project_11/views/widgets/custom_note_item.dart';
 
 class NotesListView extends StatelessWidget {
@@ -6,17 +9,23 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: ListView.builder(
-        padding: EdgeInsets.zero,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(vertical: 8),
-            child: NoteItem(),
-          );
-        },
-      ),
+    List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes ?? [];
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: ListView.builder(
+            itemCount: notes.length,
+            padding: EdgeInsets.zero,
+            itemBuilder: (context, index) {
+              return const Padding(
+                padding: EdgeInsets.symmetric(vertical: 8),
+                child: NoteItem(),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
